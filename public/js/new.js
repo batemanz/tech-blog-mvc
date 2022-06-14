5ead490ca31423ea799947dd3f53c0d1b76d79c5
Nathan Kilcrease <natekilcrease@gmail.com>
2022-06-14 15:34:57 -0700

    debugging

--- public/js/new.js
+++ public/js/new.js
@@ -13,20 +13,14 @@ const newFormHandler = async (event) => {
 //
 //
 //
-
-if (title && content) {
-  const response = await fetch("/api/post", {
-    method: "POST",
-    body: JSON.stringify({ title, content }),
-    headers: { "Content-Type": "application/json" },
+  await fetch(`/api/post`, {
+    method: 'POST',
+    body: JSON.stringify({
+      title,
+      content,
+    }),
+    headers: { 'Content-Type': 'application/json' },
   });
-
-  if (response.ok) {
-    location.reload();
-  } else {
-    alert(response.statusText);
-  }
-}
 };
 //
 //
